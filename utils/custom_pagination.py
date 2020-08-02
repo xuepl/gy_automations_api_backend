119711ac6b72ed4291d91298731e4f61d635004e
xuepl <xuepl@guoyasoft.com>
2020-08-02 16:34:06 +0800

    调整分页器默认每页显示为10页

--- utils/custom_pagination.py
+++ utils/custom_pagination.py
@@ -2,7 +2,7 @@ from rest_framework import pagination
 
 class PageNumberPagination(pagination.PageNumberPagination):
   """查第n页，每页显示n条数据"""
-  page_size = 5 # 指定每页显示多少条数据
+  page_size = 10 # 指定每页显示多少条数据
   page_size_query_param = 'size' # URL参数中每页显示条数的参数
   page_query_param = 'page' # URL中页码的参数
   max_page_size = 100 # 每页最多显示多少条数据

40d767e80262a468b25d8daa082e61b06cf43690
xuepl <xuepl@guoyasoft.com>
2020-10-29 19:26:29 +0800

    docker数据库配置

--- gy_automations_api/settings.py
+++ gy_automations_api/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
     # }
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'HOST': '127.0.0.1',  # 数据库主机
+        'HOST': 'mysql',  # 数据库主机
         'PORT': 3306,  # 数据库端口
         'USER': 'root',  # 数据库用户名
         'PASSWORD': 'root',  # 数据库用户密码
